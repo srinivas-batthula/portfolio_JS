bfffcf5d73303e7d756705eb53c6239930e62893
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2024-11-12 08:33:19 +0530

    Update Projects.js

--- components/Projects.js
+++ components/Projects.js
@@ -20,8 +20,8 @@ export default function ProjectsPage() {
                     <div className={styles.card}>
                         <div className={styles.img}>
                             {/* <img src='./../public/utils/atm_project.png' alt="project image" /> */}
-                            // <div className={styles.img2}></div>
-                                <Image src="/utils/atm_project.png" className={styles.img} />
+                            {/* <div className={styles.img2}></div> */}
+                                <Image src="/utils/atm_project.png" className={styles.img2} />
 
                             {/* <div className={styles.img2} style={{ backgroundImage: "url('./../public/utils/atm_project.png')" }}></div> */}
                         </div>
