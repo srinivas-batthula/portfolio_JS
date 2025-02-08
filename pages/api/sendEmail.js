c43d4df0f7d31057248a97771a440071420687d9
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2025-02-08 16:58:17 +0530

    Update sendEmail.js

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -4,6 +4,7 @@ import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
     if (req.method !== "POST") {
+        console.log(req);
         return res.status(405).json({ error: "Method Not Allowed" });
     }
 
