ad47345cd9bc86cffb6cc4401d1fd0f01456321e
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2025-04-09 18:31:19 +0530

    0.7.9

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://portfolio-phi-three-63.vercel.app', // replace with your domain
+    generateRobotsTxt: true,
+    changefreq: 'weekly',
+    priority: 0.7,
+    sitemapSize: 7000,
+}
