024b584906556c335a15c52c0f9f2d33c11c9b20
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2025-04-10 17:02:59 +0530

    0.8.6

--- pages/_document.js
+++ pages/_document.js
@@ -38,31 +38,33 @@ class MyDocument extends Document {
                     <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
 
                     {/* Structured Data (JSON-LD)... */}
-                    <script type="application/ld+json">
-                    {`
-                        {
-                            "@context": "https://schema.org",
-                            "@type": "Person",
-                            "name": "Srinivas Batthula",
-                            "jobTitle": "MERN Stack Developer, Full Stack Developer, Backend Developer, SDE, Software Developer",
-                            "url": "https://portfolio-phi-three-63.vercel.app",
-                            "sameAs": [
-                            "https://www.linkedin.com/in/srinivas-batthula/",
-                            "https://github.com/srinivas-batthula/",
-                            "https://leetcode.com/u/srinivas-batthula/",
-                            "https://www.instagram.com/srinivas_abhi8/",
-                            "https://x.com/Abhi07082005/"
-                            ],
-                            "worksFor": {
-                                "@type": "Organization",
-                            "name": "Sreenidhi Institute of Science and Technology, Hyderabad"
-                            },
-                            "skills": "MERN Stack, Full Stack, React, NextJs, Node.js, Express.js, FastAPI, MongoDB, SQL, JavaScript, Java, Python",
-                            "image": "https://portfolio-phi-three-63.vercel.app/icon.png"
-                        }
-                    `}
-                    </script>
-
+                    <script
+                        type="application/ld+json"
+                        dangerouslySetInnerHTML={{
+                            __html: JSON.stringify({
+                                "@context": "https://schema.org",
+                                "@type": "Person",
+                                "name": "Srinivas Batthula",
+                                "jobTitle":
+                                    "MERN Stack Developer, Full Stack Developer, Backend Developer, SDE, Software Developer",
+                                "url": "https://portfolio-phi-three-63.vercel.app",
+                                "sameAs": [
+                                    "https://www.linkedin.com/in/srinivas-batthula/",
+                                    "https://github.com/srinivas-batthula/",
+                                    "https://leetcode.com/u/srinivas-batthula/",
+                                    "https://www.instagram.com/srinivas_abhi8/",
+                                    "https://x.com/Abhi07082005/"
+                                ],
+                                "worksFor": {
+                                    "@type": "Organization",
+                                    "name": "Sreenidhi Institute of Science and Technology, Hyderabad"
+                                },
+                                "skills":
+                                    "MERN Stack, Full Stack, React, NextJs, Node.js, Express.js, FastAPI, MongoDB, SQL, JavaScript, Java, Python",
+                                "image": "https://portfolio-phi-three-63.vercel.app/icon.png"
+                            })
+                        }}
+                    ></script>
 
                 </Head>
                 <body>
